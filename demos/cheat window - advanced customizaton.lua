0c98f3d8af0eff0101bbcf4bbe834b72c56a284b
Depso <86912923+depthso@users.noreply.github.com>
2025-02-26 18:20:45 +0000

    Updated to renamed theme tags

--- demos/cheat window - advanced customizaton.lua
+++ demos/cheat window - advanced customizaton.lua
@@ -4,26 +4,28 @@ local ReGui = require(game.ReplicatedStorage.ReGui)
 ReGui:Init()
 ReGui:DefineTheme("Cherry", {
 	TitleAlign = Enum.TextXAlignment.Center,
-	InputsBg = Color3.fromRGB(25, 20, 25),
-	CheckboxBg = Color3.fromRGB(25, 20, 25),
-	ComboBg = Color3.fromRGB(25, 20, 25),
-	ActiveTabBg = Color3.fromRGB(150, 100, 150),
-	InputsGrabColor = Color3.fromRGB(150, 100, 150),
+	TextDisabled = Color3.fromRGB(120, 100, 120),
+	Text = Color3.fromRGB(200, 180, 200),
+	
+	FrameBg = Color3.fromRGB(25, 20, 25),
+	FrameBgTransparency = 0.4,
+	FrameBgActive = Color3.fromRGB(120, 100, 120),
+	FrameBgTransparencyActive = 0.4,
+	
+	CheckMark = Color3.fromRGB(150, 100, 150),
+	SliderGrab = Color3.fromRGB(150, 100, 150),
 	ButtonsBg = Color3.fromRGB(150, 100, 150),
 	CollapsingHeaderBg = Color3.fromRGB(150, 100, 150),
-	CheckboxTick = Color3.fromRGB(150, 100, 150),
+	CollapsingHeaderText = Color3.fromRGB(200, 180, 200),
+	RadioButtonHoveredBg = Color3.fromRGB(150, 100, 150),
+	
 	WindowBg = Color3.fromRGB(35, 30, 35),
 	TitleBarBg = Color3.fromRGB(35, 30, 35),
-	ActiveTitleBarBg = Color3.fromRGB(50, 45, 50),
+	TitleBarBgActive = Color3.fromRGB(50, 45, 50),
+	
 	Border = Color3.fromRGB(50, 45, 50),
-	TabBg = Color3.fromRGB(25, 20, 25),
-	Text = Color3.fromRGB(200, 180, 200),
 	ResizeGrab = Color3.fromRGB(50, 45, 50),
-	CollapsingHeaderText = Color3.fromRGB(200, 180, 200),
-	TextDisabled = Color3.fromRGB(120, 100, 120),
 	RegionBgTransparency = 1,
-	TabsBarBg = Color3.fromRGB(150, 100, 150), -- CUSTOM
-	TabPadding = UDim.new(0, 0),
 })
 
 --// Tabs
@@ -70,38 +72,38 @@ local TabsBar = Group:List({
 	Size = UDim2.new(0, 40, 1, 0),
 	CornerRadius = UDim.new(0, 5)
 })
-local TabsBox = Group:TabsBox({
+local TabSelector = Group:TabSelector({
 	NoTabsBar = true,
 	Size = UDim2.fromScale(0.5, 1)
 })
 
 local function CreateTab(Name: string, Icon)
-	local Tab = TabsBox:CreateTab({
+	local Tab = TabSelector:CreateTab({
 		Name = Name
 	})
-	
+
 	local List = Tab:List({
 		HorizontalFlex = Enum.UIFlexAlignment.Fill,
 		UiPadding = 1,
 		Spacing = 10
 	})
-	
+
 	local Button = TabsBar:Image({
 		Image = Icon,
 		Ratio = 1,
 		RatioAxis = Enum.DominantAxis.Width,
 		Size = UDim2.fromScale(1, 1),
 		Callback = function(self)
-			TabsBox:SetActiveTab(Tab)
+			TabSelector:SetActiveTab(Tab)
 		end,
 	})
-	
+
 	ReGui:SetItemTooltip(Button, function(Canvas)
 		Canvas:Label({
 			Text = Name
 		})
 	end)
-	
+
 	return List
 end
 
@@ -112,11 +114,11 @@ local function CreateRegion(Parent, Title)
 		BorderThickness = 1,
 		CornerRadius = UDim.new(0, 5)
 	})
-	
+
 	Region:Label({
 		Text = Title
 	})
-	
+
 	return Region
 end
 
@@ -179,7 +181,7 @@ AimbotSection:Combo({
 	Items = {"CFrame", "mousemoverel"},
 	Selected = 1,
 	Callback = function(self, Value)
-		
+
 	end
 })
 
@@ -188,7 +190,7 @@ AimbotSection:Combo({
 	Items = {"Head", "Torso", "Random"},
 	Selected = 1,
 	Callback = function(self, Value)
-		
+
 	end
 })
 
@@ -197,7 +199,7 @@ AimbotSection:Keybind({
 	Value = Enum.KeyCode.MouseRightButton,
 	IgnoreGameProcessed = true,
 	Callback = function(self, KeyCode)
-		
+
 	end,
 })
 
@@ -207,7 +209,7 @@ AimbotSection:SliderInt({
 	Minimum = 0,
 	Maximum = 720,
 	Callback = function(self, Value)
-		
+
 	end
 })
 
@@ -217,7 +219,7 @@ AimbotSection:SliderInt({
 	Minimum = 1,
 	Maximum = 10,
 	Callback = function(self, Value)
-		
+
 	end
 })
 
@@ -226,7 +228,7 @@ AimbotSection:SliderInt({
 	Minimum = 1,
 	Maximum = 5,
 	Callback = function(self, Value)
-		
+
 	end
 })
 
